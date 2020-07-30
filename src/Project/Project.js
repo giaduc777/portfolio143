ba51799777cc4cc553eddee6e618838b97ddb09c
Duc Le <giaduc777@yahoo.com>
2020-07-29 23:24:23 -0700

    7-29-20

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,56 +1,85 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Project.css';
 import storeFront from '../assets/storefront2.jpg';
 
-const Project = () => {
+class Project extends Component{
 
-    const getUrl = () => {
-        console.log("polo")
-         window.open("https://fullstack-restaurant.herokuapp.com/");
+    getUrl = (url) => {
+
+         let list = [
+             {
+                 fullstackRestaurant: "https://fullstack-restaurant.herokuapp.com/"
+             },
+             {
+                 mountainTravel: "https://giaduc777.github.io/Mountain-travel-10/"
+             },
+             {
+                 technicalDocumentation: "https://giaduc777.github.io/Technical-doc/"
+             },
+             {
+                 productLandingpage: "https://giaduc777.github.io/Product-landing-page/"
+             },
+             {
+                 tributePage: "https://giaduc777.github.io/Tribute-page/"
+             }
+         ]
+
+         list.forEach(name => {
+             if(Object.keys(name)[0] === url){
+                 console.log(name[url])
+               window.open(name[url]);
+             }
+         })
+
+         //window.open(url);
     }
 
-    return(
-        <div className={classes.Project}>
-            <div className={classes.title}>Project</div>
-            <div className={classes.container}>
-                 <div>
-                    <p>Fullstack Application</p>
-                    <div onClick={getUrl} className={classes.fastfood}>
-                        <img src={storeFront}></img>
-                    </div>
-                 </div>
-
-                 <div>
-                    <p>App-2</p>
-                    <div onClick={getUrl} className={classes.fastfood}>
-                        <img src={""}></img>
-                    </div>
-                 </div>
-
-                 <div>
-                    <p>App-3</p>
-                    <div onClick={getUrl} className={classes.fastfood}>
-                        <img src={""}></img>
-                    </div>
-                 </div>
-
-                 <div>
-                    <p>App-4</p>
-                    <div onClick={getUrl} className={classes.fastfood}>
-                        <img src={""}></img>
-                    </div>
-                 </div>
-
-                 <div>
-                    <p>App-5</p>
-                    <div onClick={getUrl} className={classes.fastfood}>
-                        <img src={""}></img>
-                    </div>
-                 </div>
+    render(){
+        return(
+            <div className={classes.Project}>
+                <div className={classes.title}>Project</div>
+                <div className={classes.container}>
+                     <div>
+                        <p>Fullstack Application</p>
+                        <div onClick={() => this.getUrl("fullstackRestaurant")} className={classes.fastfood}>
+                            <img src={storeFront}></img>
+                        </div>
+                     </div>
+    
+                     <div>
+                        <p>Mountain Travel</p>
+                        <div onClick={() => this.getUrl("mountainTravel")} className={classes.fastfood}>
+                            <img src={""}></img>
+                        </div>
+                     </div>
+    
+                     <div>
+                        <p>Technical Documentation</p>
+                        <div onClick={() => this.getUrl("technicalDocumentation")} className={classes.fastfood}>
+                            <img src={""}></img>
+                        </div>
+                     </div>
+    
+                     <div>
+                        <p>Product Landing Page</p>
+                        <div onClick={() => this.getUrl("productLandingpage")} className={classes.fastfood}>
+                            <img src={""}></img>
+                        </div>
+                     </div>
+    
+                     <div>
+                        <p>Tribute Page</p>
+                        <div onClick={() => this.getUrl("tributePage")} className={classes.fastfood}>
+                            <img src={""}></img>
+                        </div>
+                     </div>
+                </div>
+    
             </div>
+        )
+    }
 
-        </div>
-    )
+    
 }
 
 export default Project;
