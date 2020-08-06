a65fab119d96768b740c8e08f8882cca23476050
Duc Le <giaduc777@yahoo.com>
2020-08-06 10:42:41 -0700

    finish all media-query

--- src/About/About.js
+++ src/About/About.js
@@ -4,8 +4,10 @@ import classes from './About.css';
 const About = () => {
     return(
         <div id="about" className={classes.About}>
-              <div className={classes.title}>About me</div>
-              <p>Hello everyone, and welcome. My name is Duc Le. I study computer programming & Information Technology at Mission College in Santa Clara. After I earned my A.S degree in Information Technology, I decided to pursuit Web-Development as a career. My projects included many front-end & back-end technologies. As my skills continue to grow, more projects will be added & update in the future. Thanks for stopping by, & happy coding.</p>
+              <div className={classes.container}>
+                  <div className={classes.title}>About me</div>
+                  <p>Hello everyone, and welcome. My name is Duc Le. I study computer programming & Information Technology at Mission College in Santa Clara. After I earned my A.S degree in Information Technology, I decided to pursuit Web-Development as a career. My projects included many front-end & back-end technologies. As my skills continue to grow, more projects will be added & update in the future. Thanks for stopping by, & happy coding.</p>
+              </div>
         </div>
     )
 }
