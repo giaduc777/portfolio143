25056fa31a53363e3a525a3f9b3ef2303093aa5e
duc le <ducle@ducs-MacBook-Pro.local>
2020-08-25 22:38:02 -0700

    added tank.js project

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -11,9 +11,6 @@ const Experience = () => {
                             <i className="fab fa-node"></i>
                             <i className="fab fa-css3-alt"></i>
                         </div>
-                    
-                        
-                    
                         <div className={classes.group}>
                             <i className="fab fa-git"></i>
                             <i className="fab fa-html5"></i>
